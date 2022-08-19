21e8b9b0e03d0eefbe675255ba9f4a23d02e20fb
Alan Benavides <bruno@coastt.org>
2022-08-19 00:21:50 -0400

    add: display gifs by category

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from 'react';
+import { getGifs } from '../helpers/getGifs';
+import { GifItem } from './GifItem';
+
 export const GifGrid = ({ category }) => {
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(() => {
+    getGifs(category).then((gifs) => {
+      setGifs(gifs);
+    });
+  }, [category]);
+
   return (
     <div>
       <h3>{category}</h3>
+      <div className='card-grid'>
+        {gifs.map(({ id, url, title }) => (
+          <GifItem key={id} id={id} url={url} title={title} />
+        ))}
+      </div>
     </div>
   );
 };
