8e56d06aba505c5a27245cae3ced3d6a264c2d84
Alan Benavides <bruno@coastt.org>
2022-08-18 23:54:39 -0400

    add: add categories by form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+import { AddCategory } from './components/AddCategory';
+
 function App() {
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+
+  const addCategories = (newCategory) => {
+    setCategories([...categories, newCategory]);
+  };
+
   return (
     <div>
       <h1>GifExpertApp</h1>
+
+      <AddCategory onAddCategories={addCategories} />
+
+      <ul>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+export const AddCategory = ({ onAddCategories }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.trim().length > 2) {
+      onAddCategories(inputValue);
+      setInputValue('');
+    }
+  };
+
+  return (
+    <form style={{ display: 'flex', gap: 16 }} onSubmit={handleSubmit}>
+      <input placeholder='Buscar gifs' value={inputValue} onChange={handleInputChange} />
+      <button type='submit'>Agregar</button>
+    </form>
+  );
+};
+
+AddCategory.propTypes = {
+  onAddCategories: PropTypes.func.isRequired,
+};
